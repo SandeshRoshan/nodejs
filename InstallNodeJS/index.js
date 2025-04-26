c88ca8d84dd750707d2c7c932b4e1691e244619b
SandeshRoshan <srmilaptop@outlook.com>
2025-04-26 14:16:31 +0530

    Adding Intallation step

---
+++ InstallNodeJS/index.js
@@ -0,0 +1,19 @@
+//Installing Nodejs
+
+"step 1. nodejs in google and LTS version. "
+"Step 2. Check it. In cmd enter"
+// PS C:\Users\srmil\Desktop\NodeJs Learning> node -v
+// v22.8.0
+// PS C:\Users\srmil\Desktop\NodeJs Learning> 
+
+"Run NodeJs by writing"
+
+// PS C:\Users\srmil\Desktop\NodeJs Learning> node
+// Welcome to Node.js v22.8.0.
+// Type ".help" for more information.
+// >
+
+"Installing nodemon from npm website"
+//npm init to add package in a folder
+
+
---
+++ NodeIntro/index.js
@@ -0,0 +1,2 @@
+"Node.js is an open-source cross-platform javaScript runtime enviroment that executes JavaScripts code outside of a browser." 
+
